826a19a0086b7d6c7ec1b26b8736a223f90cf571
spiderpig60 <81110074+spiderpig60@users.noreply.github.com>
2021-04-26 14:24:16 +0300

    Add files via upload

---
+++ MySQL Script/2 CategoriesDB.sql
@@ -0,0 +1,11 @@
+-- Inserting values for the categories table
+INSERT INTO coupon.categories (name) VALUES ('FOOD');
+INSERT INTO coupon.categories (name) VALUES ('ELECTRICITY');
+INSERT INTO coupon.categories (name) VALUES ('RESTAURANT');
+INSERT INTO coupon.categories (name) VALUES ('VACATION');
+INSERT INTO coupon.categories (name) VALUES ('SPORT');
+INSERT INTO coupon.categories (name) VALUES ('AUTOMOBILE');
+INSERT INTO coupon.categories (name) VALUES ('GAMING');
+INSERT INTO coupon.categories (name) VALUES ('ATTRACTION');
+
+SELECT * FROM coupon.categories;
